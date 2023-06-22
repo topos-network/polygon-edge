3ec502632380f6efed20e8e60fffba4d86c85f39
Marko Atanasievski <markoatana@gmail.com>
2023-06-22 16:56:31 +0200

    fix: review

--- server/server.go
+++ server/server.go
@@ -608,6 +608,7 @@ func (j *jsonRPCHub) GetAccount(root types.Hash, addr types.Address) (*jsonrpc.A
 	return account, nil
 }
 
+// TODO: use eth_getProof once it is available in Polygon Edge
 func (j *jsonRPCHub) GetAccountProof(root types.Hash, addr types.Address) ([][]byte, error) {
 	proof, err := getAccountProofImpl(j.state, root, addr)
 	if err != nil {
@@ -638,6 +639,7 @@ func (j *jsonRPCHub) GetStorage(stateRoot types.Hash, addr types.Address, slot t
 	return res.Bytes(), nil
 }
 
+// TODO: use eth_getProof once it is available in Polygon Edge
 func (j *jsonRPCHub) GetStorageProof(stateRoot types.Hash, addr types.Address, slot types.Hash) ([][]byte, error) {
 	account, err := getAccountImpl(j.state, stateRoot, addr)
 	if err != nil {
